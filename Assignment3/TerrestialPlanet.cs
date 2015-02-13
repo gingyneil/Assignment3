5a84f2baa1c8932e9fa3f41147445f548316058e
gingyneil <neil.reading.nr@gmail.com>
2015-02-13 14:58:11 -0500

    Added TerrestialPlanet Class as well as IHasRing and IHasMoons Interfaces

---
+++ Assignment3/TerrestialPlanet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class TerrestialPlanet : Planet
+    {
+        //PRIVATE INSTANCE VARIALBLES+++++++++++++++++++++++++++++++++++++++++++++
+        bool _oxygen;
+
+
+        public TerrestialPlanet(bool oxygen, string name, double diameter, double mass) :base(name, diameter,mass)
+        {
+            this._oxygen = oxygen;
+        }
+
+    }
+}
